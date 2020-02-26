12b571246e82b61e970715efeb437e0420c9a1bd
Mauro Vargas <maurovargas@MacBook-Air-de-Mauro.local>
2020-02-25 19:48:28 -0500

    Refactorización

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,7 @@
 const express = require('express');
+
 const router = express.Router();
-const UserController = require('../controllers/userController');
 
+router.use(require('./UsersRoutes'));
 
-router.get('/get/users/', UserController.controllerGetAllUsers);
-router.get('/get/users/:id', UserController.controllerGetSingleUser);
-router.post('/create/users', UserController.controllerCreateNewUser);
-router.patch('/update/users/:id', UserController.controllerUpdateUser);
-router.delete('/update/users/:id', UserController.controllerDeleteUser);
-
-
-
-module.exports = { router };
+module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,82 +1,27 @@
 const express = require('express');
+
 const server = express();
 const bodyParser = require('body-parser');
-const { router } = require('../routes');
+const routes = require('../routes');
+
 const PORT = process.env.PORT || 3000;
 
+const { getRequestDate } = require('../utils');
 
-// parse application/x-www-form-urlencoded
+// parse application/x-www-form-urlencoded and application/json
 server.use(bodyParser.urlencoded({ extended: false }));
- 
-// parse application/json
 server.use(bodyParser.json());
-server.use('/', router);
-// Models
-const { Users } = require('../models');
 
-// Endpoints
+// show the time the request was made
+server.use(getRequestDate);
 
-// server.get('/users', (req, res) => {
-//   Users.find()
-//     .then((users) => res.status(200).json({ message: 'Users obtained!', data: users }))
-//     .catch((err) => res.status(404).json({ message: 'Error obtaining users', data: err }));
-// });
+// import all routes
+server.use('/api/v1', routes);
 
+// root endpoint
+server.get('/', (req, res) => res.send('Hello World!'));
 
-// server.get('/users/:id', (req, res) => {
-//   const { id } = req.params;
-
-//   Users.findById(id, req.body)
-//     .then((users) => res.status(200).json({ message: 'User obtained!', data: users }))
-//     .catch((err) => res.status(404).json({ message: 'Error obtaining user', data: err }));
-// });
-
-
-// server.patch('/users/:id', (req, res) => {
-//   const { id } = req.params;
-
-//   Users.findByIdAndUpdate(id, req.body, { new: true })
-//     .then((updatedUser) => res.status(200).json({ message: 'User updated!', data: updatedUser }))
-//     .catch((err) => res.status(404).json({ message: 'Error updating user', data: err }));
-// });
-
-
-// server.delete('/users/:id', (req, res) => {
-//   const { id } = req.params;
-
-//   Users.findByIdAndUpdate(id, { is_active: false, new: true })
-//     .then((userDeleted) => res.status(200).json())
-//     .catch((err) => res.status(404).json({ message: 'Error deleting user', data: err }));
-// });
-
-
-// CRUD Users
-// server.post('/users', (req, res) => {
-//   console.log(req.body);
-
-//   // const first_name = req.body.first_name;
-//   // const last_name = req.body.last_name;
-//   // const email = req.body.email;
-
-//   const { first_name,
-//      last_name,
-//      email 
-//     } = req.body;
-
-//   const document = {
-//     first_name,
-//     last_name,
-//     email,
-//   };
-
-//   Users.create(document)
-//     .then((user) => res.status(201).json({ message: 'User created!', data: user }))
-//     .catch((err) => res.status(400).json({ message: 'Error creating user', errors: err }));
-
-// })
-
-
-module.exports = { 
+module.exports = {
   server,
-  PORT
-}; 
+  PORT,
+};
